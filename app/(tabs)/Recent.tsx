6a3bd1eeab8501174ee9bf671d24ea51b78a78be
anktzz <ankitgupta271105@gmail.com>
2025-07-14 12:41:51 +0530

    renamed index to Recent

--- app/(tabs)/Recent.tsx
+++ app/(tabs)/Recent.tsx
@@ -1,7 +1,7 @@
 // import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Recent() {
     return (
         <View
             style={{
